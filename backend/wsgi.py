a7d417f411761d1c27a52fbff81b8bc10867dbc8
Daniil <77277774+shamemask@users.noreply.github.com>
2023-04-26 06:35:56 +0300

    localhost

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,4 +2,4 @@ from manage import app
 from eventlet import wsgi, listen
 
 if __name__ == '__main__':
-    wsgi.server(listen(('0.0.0.0', 5000)), app)
+    wsgi.server(listen(('localhost', 5000)), app)
